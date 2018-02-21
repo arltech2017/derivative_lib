5e310ad53f3e1c6a421670e92d2af3374ffb7798
Marco Sirabella <marco@sirabella.org>
2018-02-21 12:53:18 -0500

    Add start of actual source code
    (mostly just an example for cclose)

---
+++ src/derivative_lib/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+__appname__     = "__init__"
+__author__      = "Marco Sirabella"
+__copyright__   = ""
+__credits__     = ["Marco Sirabella"]  # Authors and bug reporters
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainers__ = "Marco Sirabella"
+__email__       = "marco@sirabella.org"
+__status__      = "Prototype"  # "Prototype", "Development" or "Production"
+__module__      = ""
+
+
+class Symbol():
+    pass
+
+
+class Equation():
+    pass
