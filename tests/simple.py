b6da94e27c04baff7a8fa720597208269425a43b
Marco Sirabella <marco@sirabella.org>
2018-02-27 12:17:07 -0500

    Add __eq__ and __repr__ tests

--- tests/simple.py
+++ tests/simple.py
@@ -70,6 +70,12 @@ class _Addition(unittest.TestCase):
     def test__init__(self, a, b):
         Addition(a, b)
 
+    def test__eq__(self, a, b):
+        self.assertEqual(a + b, Addition(a, b))
+
+    def test__repr__(self, a, b):
+        self.assertEqual(repr(a) + ' + ' + repr(b), repr(Addition(a, b)))
+
 
 """
 class _Expression(unittest.TestCase):
