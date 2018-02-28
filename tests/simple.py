c91eeacedc54abf4e1787834601bdeaf21d67390
Marco Sirabella <marco@sirabella.org>
2018-02-28 07:53:15 -0500

    Unlist valid_numbers

--- tests/simple.py
+++ tests/simple.py
@@ -70,7 +70,7 @@ class _Addition(unittest.TestCase):
                      st.text(string.ascii_letters, min_size=1)) for _ in
                      range(2)]
 
-    valid_numbers = [(st.integers() | st.floats(allow_nan=False))]
+    valid_numbers = (st.integers() | st.floats(allow_nan=False))
 
     @given(*valid_symbols)
     def test__init__(self, a, b):
@@ -80,16 +80,16 @@ class _Addition(unittest.TestCase):
     def test__repr__(self, a, b):
         self.assertEqual(str(a) + ' + ' + str(b), repr(Addition(a, b)))
 
-    @given(*(valid_numbers * 2))
+    @given(*[valid_numbers] * 2)
     def test__eq__(self, a, b):
         self.assertEqual(a + b, Addition(a, b))
 
-    @given(*(valid_numbers) * 2)
+    @given(*[valid_numbers] * 2)
     def test_implicit_init(self, a, b):
         self.assertIsInstance(Symbol(a) + Symbol(b), Addition)
         self.assertEqual(Addition(a, b), Symbol(a) + Symbol(b))
 
-    @given(*(valid_numbers * 5))
+    @given(*[valid_numbers] * 5)
     def test__add__(self, a, b, c, d, e):
         addition_expression1 = Addition(a, b)
         addition_expression2 = Addition(c, d)
