63268c27e7ee1371124c43872d33001eb1c9ceae
Marco Sirabella <marco@sirabella.org>
2018-03-16 11:23:42 -0400

    Fix wierd python list comprehension namespace thing

--- tests/simple.py
+++ tests/simple.py
@@ -68,9 +68,8 @@ class _Addition(unittest.TestCase):
 
     valid_numbers = (st.integers() | st.floats(allow_nan=False,
                                                allow_infinity=False))
-    valid_symbols = [(valid_numbers |
-                     st.text(string.ascii_letters, min_size=1)) for _ in
-                     range(2)]
+    valid_symbols = [(valid_numbers | st.text(string.ascii_letters,
+                                              min_size=1))] * 2
 
     @given(*valid_symbols)
     def test__init__(self, a, b):
