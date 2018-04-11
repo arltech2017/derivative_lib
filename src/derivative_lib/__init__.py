2983d90b331534e4a293fedfd9cb91eda57de871
CassiusClose <CassiusClose@gmail.com>
2018-04-11 12:02:59 -0400

    Added simplify methods

--- src/derivative_lib/__init__.py
+++ src/derivative_lib/__init__.py
@@ -32,19 +32,19 @@ class Symbol():
         return str(self.data) == str(normalize(op).data)
 
     def __add__(self, op):
-        return Addition(self, op)
+        return Addition(self, op).simplify()
 
     def __sub__(self, op):
-        return Subtraction(self, op)
+        return Subtraction(self, op).simplify()
 
     def __mul__(self, op):
-        return Multiplication(self, op)
+        return Multiplication(self, op).simplify()
 
     def __truediv__(self, op):
-        return Division(self, op)
+        return Division(self, op).simplify()
 
     def __pow__(self, op):
-        return Power(self, op)
+        return Power(self, op).simplify()
 
     def __str__(self):
         s = ""
@@ -61,7 +61,10 @@ class Symbol():
     def __neg__(self):
         s = Symbol(self.data[0])
         s.negative = self.negative ^ True
-        return s 
+        return s
+
+    def derivative(self):
+        return Number(1)
 
 
 class Number(Symbol):
@@ -99,6 +102,9 @@ class Number(Symbol):
     def __neg__(self):
         return Number(-self.data[0])
 
+    def derivative(self):
+        return Number(0)
+
 
 class Addition(Symbol):
     def __init__(self, op1, op2):
@@ -113,12 +119,36 @@ class Addition(Symbol):
     def __neg__(self):
         return Subtraction(-self.data[0], self.data[1])
 
+    def simplify(self):
+        if self.data[0] == 0:
+            return self.data[1]
+        elif self.data[1] == 0:
+            return self.data[0]
+        elif self.data[0] == self.data[1]:
+            return self.data[0] * Number(2)
+        return self
+
+    def derivative(self):
+        return self.data[0].derivative() + self.data[1].derivative()
+
 class Subtraction(Addition):
     def __init__(self, op1, op2):
         self.data = (normalize(op1), -normalize(op2))
 
     def __str__(self):
         return "(" + str(self.data[0]) + " - " + str(-self.data[1]) + ")"
+
+    def simplify(self):
+        if self.data[0] == 0:
+            return -self.data[1]
+        elif self.data[1] == 0:
+            return self.data[0]
+        elif self.data[0] == self.data[1]:
+            return Number(0)
+        return self
+
+    def derivative(self):
+        return self.data[0].derivative() - self.data[1].derivative()
 
 class Multiplication(Symbol):
     def __init__(self, op1, op2):
@@ -133,12 +163,32 @@ class Multiplication(Symbol):
     def __neg__(self):
         return Multiplication(-self.data[0], self.data[1])
 
+    def simplify(self):
+        if self.data[0] == 0 or self.data[1] == 0:
+            return Number(0)
+        if self.data[0] == 1:
+            return self.data[1]
+        elif self.data[0] == -1:
+            return -self.data[1]
+        elif self.data[1] == 1:
+            return self.data[0]
+        elif self.data[1] == -1:
+            return -self.data[0]
+        elif self.data[0] == self.data[1]:
+            return self.data[0] ** Number(2)
+        return self
+
+    def derivative(self):
+        return (self.data[0].derivative() * self.data[1]) + (self.data[0] * self.data[1].derivative())
+
+
 class Division(Multiplication):
     def __init__(self, op1, op2):
         self.data = (normalize(op1), normalize(op2) ** -1)
 
     def __str__(self):
         return "(" + str(self.data[0]) + " / " + str(self.data[1]) + ")"
+
 
 class Power(Symbol):
     def __init__(self, op1, op2):
@@ -150,5 +200,14 @@ class Power(Symbol):
     def __eq__(self, op):
         return self.data == op.data
 
+    def simplify(self):
+        if self.data[1] == 0:
+            return Number(1)
+        if self.data[1] == 1:
+            return self.data[0]
+        if self.data[0] == 0: #must come after self.data[1] == 0
+            return Number(0)
+        return self
+
     def derivative(self):
-        return self.data[1] * (self.data[0] ** (self.data[1] - Number(1)))
+        return (self.data[1] * ((self.data[0] ** (self.data[1] - Number(1))) * self.data[1].derivative()))
